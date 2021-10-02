6a4d59a39698803eccd783d04a49fd1719e1eae6
HimanshuMS <himanshushah1215@gmail.com>
2021-10-02 23:03:09 +0530

    added initial logic for page turn effect

---
+++ lib/src/builders/turn_state.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+import '../effects/curve.dart';
+
+class PageTurnImage extends StatefulWidget {
+  const PageTurnImage({
+    Key? key,
+    required this.amount,
+    required this.image,
+    required this.backgroundColor,
+  }) : super(key: key);
+
+  final Animation<double> amount;
+  final ImageProvider image;
+  final Color backgroundColor;
+
+  @override
+  _PageTurnImageState createState() => _PageTurnImageState();
+}
+
+class _PageTurnImageState extends State<PageTurnImage> {
+  late ImageStream _imageStream;
+  late ImageInfo? _imageInfo;
+  bool _isListeningToStream = false;
+
+  late ImageStreamListener _imageListener;
+
+  @override
+  void initState() {
+    super.initState();
+    _imageListener = ImageStreamListener(_handleImageFrame);
+  }
+
+  @override
+  void dispose() {
+    _stopListeningToStream();
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    _resolveImage();
+    if (TickerMode.of(context)) {
+      _listenToStream();
+    } else {
+      _stopListeningToStream();
+    }
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(PageTurnImage oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.image != oldWidget.image) {
+      _resolveImage();
+    }
+  }
+
+  @override
+  void reassemble() {
+    _resolveImage(); // in case the image cache was flushed
+    super.reassemble();
+  }
+
+  void _resolveImage() {
+    final ImageStream newStream =
+        widget.image.resolve(createLocalImageConfiguration(context));
+    _updateSourceStream(newStream);
+  }
+
+  void _handleImageFrame(ImageInfo imageInfo, bool synchronousCall) {
+    setState(() => _imageInfo = imageInfo);
+  }
+
+  void _updateSourceStream(ImageStream newStream) {
+    if (_imageStream.key == newStream.key) return;
+
+    if (_isListeningToStream) _imageStream.removeListener(_imageListener);
+
+    _imageStream = newStream;
+    if (_isListeningToStream) _imageStream.addListener(_imageListener);
+  }
+
+  void _listenToStream() {
+    if (_isListeningToStream) return;
+    _imageStream.addListener(_imageListener);
+    _isListeningToStream = true;
+  }
+
+  void _stopListeningToStream() {
+    if (!_isListeningToStream) return;
+    _imageStream.removeListener(_imageListener);
+    _isListeningToStream = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_imageInfo != null) {
+      return CustomPaint(
+        painter: CurveEffect(
+          amount: widget.amount,
+          image: _imageInfo!.image,
+          backgroundColor: widget.backgroundColor,
+        ),
+        size: Size.infinite,
+      );
+    } else {
+      return const SizedBox();
+    }
+  }
+}
